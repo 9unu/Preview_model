dfc8c7e4d8996b6de57294070bda075f479bfa11
geonwoo park <gw5994@naver.com>
2024-05-21 13:24:49 +0900

    [ADDED] train, test, valid split

--- ocr_tagging/src_ocr/data_manager/parsers/split_csv.py
+++ ocr_tagging/src_ocr/data_manager/parsers/split_csv.py
@@ -19,7 +19,6 @@ def print_dist(d, flag):
 def file_split(args):
     # 파싱할 파일 list 구성
     file_list = [os.path.join(args.fp, file) for file in os.listdir(args.fp) if "csv" in file]
-
     # 파일을 저장할 경로가 없다면 생성
     if not os.path.exists(args.save_p):
         os.makedirs(args.save_p)
@@ -29,42 +28,36 @@ def file_split(args):
     for p in save_dir_name:
         if not os.path.exists(os.path.join(args.save_p, p)):
             os.makedirs(os.path.join(args.save_p, p))
+    print(file_list)
+    total_df = pd.read_csv(file_list[0])
+    for file in file_list[1:]:
+        last_num=total_df['Sentence #'].iloc[-1]
+        last_num=int(last_num.replace("Sentence ",""))
+        df=pd.read_csv(file)
+        Sentence_list=df['Sentence #'].unique()
+        for index, sentence in enumerate(Sentence_list, start=last_num+1):
+            df.loc[df['Sentence #'] == sentence, 'Sentence #'] = "Sentence "+str(index)
+        total_df=pd.concat([total_df, df])
 
-    for f in file_list:
-        print("now parsing {name}".format(name=f))
-        output_base_fp = f.split(args.fp)[-1].split(".csv")[0] + "_" # 저장할 파일명 format 설정
-        df = pd.read_csv(f, encoding=args.encoding)
-        df.loc[:, "Sentence #"] = df["Sentence #"].fillna(method="ffill") # 결측치 fill
+    # Sentnece ID를 기준으로 group화하여 test set 랜덤 추출
+    test_split = GroupShuffleSplit(test_size=args.test_ratio, n_splits=1,
+                                       random_state=42).split(total_df, groups=total_df['Sentence #'])
+    train_val_idxs, test_idxs = next(test_split)
 
-        # Sentnece ID를 기준으로 group화하여 test set 랜덤 추출
-        test_split = GroupShuffleSplit(test_size=args.test_ratio, n_splits=1,
-                                       random_state=42).split(df, groups=df['Sentence #'])
-        train_val_idxs, test_idxs = next(test_split)
-
-        train_val = df.iloc[train_val_idxs]
-        test = df.iloc[test_idxs]
+    train_val = total_df.iloc[train_val_idxs]
+    test = total_df.iloc[test_idxs]
 
         # Sentnece ID를 기준으로 group화하여 validation set 랜덤 추출
-        val_split = GroupShuffleSplit(test_size=args.val_ratio,
+    val_split = GroupShuffleSplit(test_size=args.val_ratio,
                                       n_splits=1, random_state=42).split(train_val, groups=train_val['Sentence #'])
-        train_idxs, val_idxs = next(val_split)
-        train = train_val.iloc[train_idxs]
-        val = train_val.iloc[val_idxs]
+    train_idxs, val_idxs = next(val_split)
+    train = train_val.iloc[train_idxs]
+    val = train_val.iloc[val_idxs]
 
         # 결과 파일 저장
-        train.to_csv(os.path.join(args.save_p + save_dir_name[0],
-                                  output_base_fp + save_dir_name[0] + ".csv"), encoding=args.encoding, index=False,
-                     line_terminator='\n')
-        val.to_csv(os.path.join(args.save_p + save_dir_name[1],
-                                output_base_fp + save_dir_name[1] + ".csv"), encoding=args.encoding, index=False,
-                   line_terminator='\n')
-        test.to_csv(os.path.join(args.save_p + save_dir_name[2],
-                                 output_base_fp + save_dir_name[2] + ".csv"), encoding=args.encoding, index=False,
-                    line_terminator='\n')
-
-
-
-
+    train.to_csv(os.path.join(args.save_p, save_dir_name[0], save_dir_name[0] + ".csv"), encoding=args.encoding, index=False)
+    val.to_csv(os.path.join(args.save_p, save_dir_name[1], save_dir_name[1] + ".csv"), encoding=args.encoding, index=False)
+    test.to_csv(os.path.join(args.save_p, save_dir_name[2], save_dir_name[2] + ".csv"), encoding=args.encoding, index=False)
 
 
 
