00a027b3c1c4688ab65291f72a236a592c12cda7
KET <ket0825@gmail.com>
2024-06-28 22:10:37 +0900

    chore : json2csv little improved by early return.

--- review_tagging/src_review/data_manager/parsers/json2csv.py
+++ review_tagging/src_review/data_manager/parsers/json2csv.py
@@ -98,6 +98,10 @@ def process_json_file(file_path, output_csv_path):
             or not item['our_topics'] 
             or 'content' not in item
             ):
+            continue    
+        
+        our_topics = clean_data(item['our_topics']) # 이 안에 regexp 들어가 있음.
+        if not our_topics:
             continue        
 
         # print(f"review_counter: {review_counter}")
@@ -105,8 +109,7 @@ def process_json_file(file_path, output_csv_path):
         # content = preprocess_text(item['content']) # 이거 빼고도 해봐야 함.
         content = more_than_one_space.sub(" ", content)
         sentences = split_content_into_sentences(content)        
-        our_topics = clean_data(item['our_topics']) # 이 안에 regexp 들어가 있음.
-        our_topics = sorted(item['our_topics'], key=lambda x: len(x['text']), reverse=True)
+        
 
         sentence_dict_list = []        
         word_index = 0
@@ -184,7 +187,7 @@ def process_json_file(file_path, output_csv_path):
         #         print("[MISMATCH]")
      
         sentence_words_concat = content                                              
-        
+        our_topics = sorted(our_topics, key=lambda x: len(x['text']), reverse=True)        
         checked_indice_set = set()
         for topic in our_topics:
             try:
@@ -270,7 +273,7 @@ def process_json_files_in_folder(folder_path, output_folder):
         os.makedirs(output_folder, exist_ok=True)
 
     # 폴더 내의 모든 JSON 파일 처리    
-    for filename in os.listdir(folder_path):        
+    for filename in os.listdir(folder_path):                
         if filename.endswith(".json"):
             json_file_path = os.path.join(folder_path, filename)
             output_csv_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}.csv")
