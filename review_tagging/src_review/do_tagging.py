9b8cc0b2a2bd79b18bf4cb67f20ea3da94610d50
KHUjsLee <jiseok1015@naver.com>
2024-09-04 22:51:24 +0900

    [MODIFIED] preprocess / tag 분리

--- review_tagging/src_review/do_tagging.py
+++ review_tagging/src_review/do_tagging.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from data_manager.loaders.loader import set_loader
 from modeling.model import ABSAModel
-from modeling.trainer import tag_fn, tag_valid_fn, valid_eval_fn ### 함수import 수정
+from modeling.trainer import tag_valid_fn, valid_eval_fn, tag_fn, preprocess_fn ### 함수import 수정
 from utils.model_utils import device_setting, load_model
 from utils.set_logger import Log
 import os
@@ -12,57 +12,6 @@ import warnings
 import transformers
 warnings.filterwarnings(action='ignore')
 
-
-def tag(config):
-    tagging_fp = config.tagging_fp
-    log_name = "tagging_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
-    logger = Log()
-    log = logger.set_log(config.base_path, filename=log_name, level="DEBUG")
-
-    model_path = os.path.join(config.base_path, config.out_model_path)
-    log.info(f"[실행 명령어]: {config.cmd}")
-    log.info("[Config]")
-    log.info(f'{config.__dict__}')
-
-    log.info('>>>>>>> Now Loading Sentiment&Aspect Category Encoder')
-    meta_data = joblib.load(os.path.join(config.base_path, config.label_info_file))
-    enc_sentiment, enc_aspect, enc_aspect2 = meta_data["enc_sentiment"], meta_data["enc_aspect"], meta_data["enc_aspect2"] ### enc_aspect2 추가함
-    enc_sentiment_score, enc_aspect_score = meta_data["enc_sentiment_score"], meta_data["enc_aspect_score"] ### score encoder 추가
-
-    num_sentiment = len(list(meta_data["enc_sentiment"].classes_))
-    num_aspect, num_aspect2 = len(list(meta_data["enc_aspect"].classes_)), len(list(meta_data["enc_aspect2"].classes_))
-    num_sentiment_score = len(list(meta_data["enc_sentiment_score"].classes_)) ### score_num 추가
-    num_aspect_score = len(list(meta_data["enc_aspect_score"].classes_)) ### score_num 추가
-
-    # Device Setting (GPU/CPU)
-    device = device_setting(log)
-    
-    start_time_creating_architecture = datetime.datetime.now()
-    # model Architecture 생성
-    model = ABSAModel(num_sentiment=num_sentiment, num_aspect=num_aspect, num_aspect2=num_aspect2,
-                      num_sentiment_score=num_sentiment_score, num_aspect_score=num_aspect_score,
-                      config=config,
-                      need_birnn=bool(config.need_birnn)) ### num_score 추가
-    end_time_creating_architecture = datetime.datetime.now()
-
-    print(f"모델 아키텍쳐 생성 시간: {end_time_creating_architecture - start_time_creating_architecture}")
-    
-
-    start_time_loading_model = datetime.datetime.now()
-    # 저장된 모델 load
-    model = load_model(model=model, state_dict_path=model_path, device=device)
-    end_time_loading_model = datetime.datetime.now()
-
-    print(f"저장된 모델 loading 시간: {end_time_loading_model - start_time_loading_model}")
-
-    # Eval DataLoader setting
-    # eval_data_loader = set_loader(config=config, fp=tagging_fp, batch_size=config.eval_batch_size, meta_data=meta_data)
-
-    tokenizer = transformers.BertTokenizer.from_pretrained(config.init_model_path, do_lower_case=False)
-    # 태깅 수행
-    tag_fn(config, tokenizer, model, enc_sentiment, enc_aspect, enc_aspect2, enc_sentiment_score, enc_aspect_score,
-            device, log)
-    ### eval_fn 형식에 맞게 score 추가
 
 
 def tag_valid(config):
@@ -129,6 +78,78 @@ def valid_eval(config):
 
 
 
+def tag(config):
+    tagging_fp = config.tagging_fp
+    log_name = "tagging_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
+    logger = Log()
+    log = logger.set_log(config.base_path, filename=log_name, level="DEBUG")
+
+    model_path = os.path.join(config.base_path, config.out_model_path)
+    log.info(f"[실행 명령어]: {config.cmd}")
+    log.info("[Config]")
+    log.info(f'{config.__dict__}')
+
+    log.info('>>>>>>> Now Loading Sentiment&Aspect Category Encoder')
+    meta_data = joblib.load(os.path.join(config.base_path, config.label_info_file))
+    enc_sentiment, enc_aspect, enc_aspect2 = meta_data["enc_sentiment"], meta_data["enc_aspect"], meta_data["enc_aspect2"] ### enc_aspect2 추가함
+    enc_sentiment_score, enc_aspect_score = meta_data["enc_sentiment_score"], meta_data["enc_aspect_score"] ### score encoder 추가
+
+    num_sentiment = len(list(meta_data["enc_sentiment"].classes_))
+    num_aspect, num_aspect2 = len(list(meta_data["enc_aspect"].classes_)), len(list(meta_data["enc_aspect2"].classes_))
+    num_sentiment_score = len(list(meta_data["enc_sentiment_score"].classes_)) ### score_num 추가
+    num_aspect_score = len(list(meta_data["enc_aspect_score"].classes_)) ### score_num 추가
+
+    # Device Setting (GPU/CPU)
+    device = device_setting(log)
+    
+    start_time_creating_architecture = datetime.datetime.now()
+    # model Architecture 생성
+    model = ABSAModel(num_sentiment=num_sentiment, num_aspect=num_aspect, num_aspect2=num_aspect2,
+                      num_sentiment_score=num_sentiment_score, num_aspect_score=num_aspect_score,
+                      config=config,
+                      need_birnn=bool(config.need_birnn)) ### num_score 추가
+    end_time_creating_architecture = datetime.datetime.now()
+
+    print(f"모델 아키텍쳐 생성 시간: {end_time_creating_architecture - start_time_creating_architecture}")
+    
+
+    start_time_loading_model = datetime.datetime.now()
+    # 저장된 모델 load
+    model = load_model(model=model, state_dict_path=model_path, device=device)
+    end_time_loading_model = datetime.datetime.now()
+
+    print(f"저장된 모델 loading 시간: {end_time_loading_model - start_time_loading_model}")
+
+    # Eval DataLoader setting
+    # eval_data_loader = set_loader(config=config, fp=tagging_fp, batch_size=config.eval_batch_size, meta_data=meta_data)
+
+    tokenizer = transformers.BertTokenizer.from_pretrained(config.init_model_path, do_lower_case=False)
+    # 태깅 수행
+    tag_fn(config, tokenizer, model, enc_sentiment, enc_aspect, enc_aspect2, enc_sentiment_score, enc_aspect_score,
+            device, log)
+    ### eval_fn 형식에 맞게 score 추가
+
+
+
+def preprocess(config):
+    log_name = "preprocessing_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".log"
+    logger = Log()
+    log = logger.set_log(config.base_path, filename=log_name, level="DEBUG")
+
+    model_path = os.path.join(config.base_path, config.out_model_path)
+    log.info(f"[실행 명령어]: {config.cmd}")
+    log.info("[Config]")
+    log.info(f'{config.__dict__}')
+
+    # Device Setting (GPU/CPU)
+    device = device_setting(log)
+
+    start_time_preprocessing = datetime.datetime.now()
+    preprocess_fn(config)
+    end_time_preprocessing = datetime.datetime.now()
+
+    print(f"전처리 시간: {end_time_preprocessing - start_time_preprocessing}")
+
 
 
 if __name__ == "__main__":
@@ -149,11 +170,15 @@ if __name__ == "__main__":
     parser.add_argument("--aspect_in_feature", type=int, default=768,
                         help="각 Aspect Category input sample의 size")
     # ===================================================
-    parser.add_argument("--tagging_fp", type=str, help="태깅할 데이터들이 포함된 디렉토리 경로 or 태깅할 데이터 파일 경로 설정", default="./resources_review/data_untagged_json/")
+    parser.add_argument("--preprocessing_fp", type=str, help="전처리할 데이터들이 포함된 디렉토리 경로 or 전처리할 데이터 파일 경로 설정", default="./resources_review/data_json/")    
+    parser.add_argument("--tagging_fp", type=str, help="태깅할 데이터들이 포함된 디렉토리 경로 or 태깅할 데이터 파일 경로 설정", default="./resources_review/preprocessed_results_json/")
     ### path 수정
     parser.add_argument("--valid_fp", type=str, help="valid 데이터 포함된 디렉토리 경로 or valid 데이터 파일 경로 설정", default="./resources_review/data_valid/")
     parser.add_argument("--base_path", type=str, help="평가를 수행할 Model과 Encoder가 저장된 경로", default="./ckpt_review/model/")
     ### 경로 수정
+
+    parser.add_argument("--need_preprocessing", type=int, default=1, help="json 데이터에 대해서 전처리를 진행해야 하는지 여부 (True: 1/False: 0)")
+
     parser.add_argument("--label_info_file", type=str, help="사용할 Encoder 파일명", default="meta.bin")
     parser.add_argument("--out_model_path", type=str, help="평가할 model의 파일명", default="pytorch_model.bin")
     parser.add_argument("--print_sample", type=int, default=1, help="각 sample의 예측 결과 출력 여부를 결정 (True: 1/False: 0)")
@@ -162,10 +187,13 @@ if __name__ == "__main__":
     config = parser.parse_args()
 
     # valid.csv에 대해서 모델이 예측한 결과를 csv로 저장
-    tag_valid(config)
+    # tag_valid(config)
 
     # tag_valid에서 나온 csv 파일과 원본 valid.csv 파일을 비교해서 성능 측정
-    valid_eval(config)
+    # valid_eval(config)
+    
+    # 전처리하는 함수
+    # preprocess(config)
     
     # json 파일들에 대해서 모델이 예측한 결과를 json으로 저장
     tag(config)
